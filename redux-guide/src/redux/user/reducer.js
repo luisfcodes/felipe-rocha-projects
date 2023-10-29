1ecf0d0b842f69d64e1eb736c3b1b7ae4bdddde3
Luis Fernando <luis.silva9902@gmail.com>
2023-10-29 11:48:04 -0300

    feat: add redux and create store, User entity and root-reducer

---
+++ redux-guide/src/redux/user/reducer.js
@@ -0,0 +1,16 @@
+const initialState = {
+  currentUser: null,
+}
+
+const userReducer = (state = initialState, action) => {
+  if(action.type === 'user/login') {
+    return {
+      ...state,
+      currentUser: 10
+    }
+  }
+
+  return state
+}
+
+export default userReducer
