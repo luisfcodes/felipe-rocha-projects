d87f9f0555d08ff400e2d5b823139d370060d695
Luis Fernando <luis.silva9902@gmail.com>
2023-10-29 15:29:49 -0300

    feat: add methods to remove, increase and decrease products

--- redux-guide/src/components/cart/index.jsx
+++ redux-guide/src/components/cart/index.jsx
@@ -1,14 +1,26 @@
 // Styles
 import * as Styles from "./styles";
+import { useSelector } from "react-redux";
+import CartItem from "../cart-item/index";
+import { selectProductsTotalPrice } from "../../redux/cart/cart.selectors";
 
 const Cart = ({ isVisible, setIsVisible }) => {
   const handleEscapeAreaClick = () => setIsVisible(false);
+  const { products } = useSelector((state) => state.cartReducer);
+  const productsCount = useSelector(selectProductsTotalPrice);
 
   return (
     <Styles.CartContainer isVisible={isVisible}>
       <Styles.CartEscapeArea onClick={handleEscapeAreaClick} />
       <Styles.CartContent>
         <Styles.CartTitle>Seu Carrinho</Styles.CartTitle>
+        {products.map((product) => (
+          <CartItem key={product.id} product={product} />
+        ))}
+
+        {products.length && (
+          <Styles.CartTotal>Total: R${productsCount}</Styles.CartTotal>
+        )}
       </Styles.CartContent>
     </Styles.CartContainer>
   );
